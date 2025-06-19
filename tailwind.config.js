3d0c0c6b4ee27528370e1c6b0b70b0f0d465a66b
siddharth-tutorial <ggvanecha@gmail.com>
2025-06-19 18:24:02 +0530

    tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ export default {
         dark: "#333333",
         dark2: "#999999",
       },
+       blur: {
+        '140': '140px',
+      },
+      spacing: {
+        '500': '500px',
+        '600': '600px',
+      },
       container: {
         center: true,
         padding: {
@@ -39,12 +46,17 @@ export default {
         blink: {
           "50%": { borderColor: "transparent" },
         },
+         float: {
+          "0%, 100%": { transform: "translateY(0px) translateX(0px)" },
+          "50%": { transform: "translateY(-10px) translateX(10px)" },
+        },
       },
       animation: {
         fadeIn: "fadeIn 0.5s ease-in-out",
         typewriter: "typewriter 2s steps(30) 2s forwards",
         blink: "blink 1s step-end infinite",
         fadeInUp: "fadeInUp 0.5s ease-out forwards",
+        float: "float 3s ease-in-out infinite",
       },
     },
   },
